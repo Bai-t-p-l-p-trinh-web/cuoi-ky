449784c233cdb1a263d76e2a323c0b8be9e3f7b8
Le Viet Xuan <xuantryingbetter@gmail.com>
2025-06-09 21:11:41 +0700

    filter car

--- frontend/src/pages/MyAccount/child/ManageCar.jsx
+++ frontend/src/pages/MyAccount/child/ManageCar.jsx
@@ -6,8 +6,9 @@ import { MdOutlineSell } from "react-icons/md";
 import { GiPayMoney } from "react-icons/gi";
 import { GrHide } from "react-icons/gr";
 import { CgDetailsMore } from "react-icons/cg";
+import { CiSearch } from "react-icons/ci";
 import { CiCirclePlus } from "react-icons/ci";
-import { RiDeleteBin5Line } from "react-icons/ri";
+import { RiDeleteBin5Line, RiSortNumberAsc, RiSortNumberDesc  } from "react-icons/ri";
 import "../scss/ManageCar.scss";
 import { Link } from "react-router-dom";
 import { convertCurrency } from "../../../utils/ConvertNumber";
@@ -18,6 +19,13 @@ function ManageCar(){
     const [cars, setCars] = useState([]);
 
     const [carSlugs, setCarSlugs] = useState([]);
+    const [originCars, setOriginCars] = useState([]);
+    const [filterCar, setFilterCar] = useState({
+        status : "",
+        search : "",
+        sort : ""
+    });
+    const resultFilter = useRef([]);
     const [carPickedSlugs, setCarPickedSlugs] = useState([]);
 
     useEffect(() => {
@@ -34,6 +42,7 @@ function ManageCar(){
                 const respondsCar = await axios.get(`${import.meta.env.VITE_SERVER_URL}/api/v1/car/display`, {
                     withCredentials : true
                 });
+                setOriginCars(respondsCar.data);
                 setCars(respondsCar.data);
             } catch(error) {
                 toast.error(error.response?.data?.message || 'Lỗi khi tìm xe');
@@ -41,6 +50,77 @@ function ManageCar(){
         };
         getCars();
     }, []);
+
+    // filters, sort, search 
+    const handleChangeStatus = (status) => {
+        setFilterCar((prev) => (
+            {
+                ...prev,
+                status
+            }
+        ));
+    }
+
+    const handleChangeSort = () => {
+        const currentSort = filterCar.sort;
+        let sort = '';
+        if(!currentSort) sort = 'asc';
+        else if(currentSort === 'asc') sort = 'desc';
+        else sort = '';
+
+        setFilterCar((prev) => (
+            {
+                ...prev,
+                sort
+            }
+        ));
+    }
+
+    const handleChangeSearch = (search) => {
+        setFilterCar((prev) => (
+            {
+                ...prev,
+                search
+            }
+        ));
+    }
+
+    const FilterByStatus = (status) => {
+        if(!status) return;
+
+        resultFilter.current = resultFilter.current.filter(car => car.status === status);
+    }
+
+    const FilterBySearch = (search) => {
+        if(!search) return;
+
+        const regex = new RegExp(search, "i");
+
+        resultFilter.current = resultFilter.current.filter(car => regex.test(car.title));
+    }
+
+    const FilterBySort = (sort) => {
+        if(!sort) return;
+
+        if(sort === 'asc') {
+            resultFilter.current = resultFilter.current.sort((a,b) => a.price - b.price);
+        } else {
+            resultFilter.current = resultFilter.current.sort((a,b) => b.price - a.price);
+        }
+    }
+
+    useEffect(() => {
+        if(!originCars || originCars.length === 0) return;
+        const {status, search, sort} = filterCar;
+        resultFilter.current = [...originCars];
+        FilterByStatus(status);
+        FilterBySearch(search);
+        FilterBySort(sort);
+
+        setCars(resultFilter.current);
+
+    }, [filterCar]);
+    // end filters, sort, search 
 
     // so sánh 2 phần tử bằng nhau (đúng thứ tự => so sánh)
     const arraysEqual = (a, b) => {
@@ -98,27 +178,27 @@ function ManageCar(){
                 <div className="manageCar__contain">
                     <div className="manageCar__options">
                         <ul className="manageCar__status__list">
-                            <li className="manageCar__status">
+                            <li className={`manageCar__status ${!filterCar.status ? 'picked' : ''}`} onClick={() => {handleChangeStatus('')}}>
                                 <PiCheckCircleLight className="manageCar__status__svg" />
                                 <span className="manageCar__status__span">Tất cả</span>
                             </li>
 
-                            <li className="manageCar__status">
+                            <li className={`manageCar__status ${filterCar.status === 'selling' ? 'picked' : ''}`} onClick={() => {handleChangeStatus('selling')}}>
                                 <MdOutlineSell className="manageCar__status__svg" />
                                 <span className="manageCar__status__span">Đang bán</span>
                             </li>
 
-                            <li className="manageCar__status">
+                            <li className={`manageCar__status ${filterCar.status === 'deposited' ? 'picked' : ''}`} onClick={() => {handleChangeStatus('deposited')}}>
                                 <GiPayMoney className="manageCar__status__svg" />
                                 <span className="manageCar__status__span">Đặt cọc</span>
                             </li>
 
-                            <li className="manageCar__status">
+                            <li className={`manageCar__status ${filterCar.status === 'hiden' ? 'picked' : ''}`} onClick={() => {handleChangeStatus('hidden')}}>
                                 <GrHide className="manageCar__status__svg" />
                                 <span className="manageCar__status__span">Ẩn</span>
                             </li> 
 
-                            <li className="manageCar__status">
+                            <li className={`manageCar__status ${filterCar.status === 'sold' ? 'picked' : ''}`} onClick={() => {handleChangeStatus('sold')}}>
                                 <PiMoneyThin className="manageCar__status__svg" />
                                 <span className="manageCar__status__span">Đã bán</span>
                             </li>
@@ -126,10 +206,28 @@ function ManageCar(){
 
                         <div className="manageCar__filters">
                             <div className="manageCar__search">
-                                Search
+                                <CiSearch className="manageCar__search__svg"/>
+                                <input type="text" name="search" className="manageCar__search__input" placeholder="Tìm kiếm..." value={filterCar.search} onChange={(e) => {handleChangeSearch(e.target.value)}} />
                             </div>
                             <div className="manageCar__sort">
-                                Sort
+                                <button className="manageCar__sort__button" onClick={handleChangeSort}>
+                                    {
+                                        !filterCar.sort
+                                        &&
+                                        <img src="/unsorted.svg" alt="unsorted" />
+                                    }
+                                    {
+                                        filterCar.sort === 'asc'
+                                        &&
+                                        <RiSortNumberAsc/>
+                                    }
+                                    {
+                                        filterCar.sort === 'desc'
+                                        &&
+                                        <RiSortNumberDesc/>
+                                    }
+                                    <span>Sort</span>
+                                </button>
                             </div>
                             <div className="manageCar__add">
                                 <Link to="request_add" className="manageCar__link">
