25d85b8c3a54ec08262c5b4eac2a52befd1daf5c
Tien Lam <nhattienlam2005@gmail.com>
2025-06-15 16:19:14 +0700

    not found page

--- frontend/src/pages/Error404/index.jsx
+++ frontend/src/pages/Error404/index.jsx
@@ -1,25 +1,32 @@
 import { useNavigate } from "react-router-dom";
+import "./Error404.css";
 
-function Error404(){
-    const navigate = useNavigate();
-    const handleRefresh = () => {
-        window.location.reload();
-    }
-    const handleHome = () => {
-        navigate("/");
-    }
-    return (
-        <>
-            <div className="error">
-                <img className="error__img" src="/error-404.svg" alt="Không tìm thấy"/>
-                <h3 className="error__title">Không tìm thấy nội dung</h3>
-                <p className="error__description">Trang bạn đang tìm kiếm có thể bị xóa hoặc không tồn tại</p>
-                <div className="error__buttons">
-                    <button className="error__refresh" onClick={handleRefresh}>Làm mới</button>
-                    <button className="error__home" onClick={handleHome}>Trang chủ</button>
-                </div>
-            </div>
-        </>
-    )
-};
+function Error404() {
+  const navigate = useNavigate();
+  const handleRefresh = () => {
+    window.location.reload();
+  };
+  const handleHome = () => {
+    navigate("/");
+  };
+  return (
+    <>
+      <div className="error">
+        <img className="error__img" src="/error-404.svg" alt="Không tìm thấy" />
+        <h3 className="error__title">Không tìm thấy nội dung</h3>
+        <p className="error__description">
+          Trang bạn đang tìm kiếm có thể bị xóa hoặc không tồn tại
+        </p>
+        <div className="error__buttons">
+          <button className="error__refresh" onClick={handleRefresh}>
+            Làm mới
+          </button>
+          <button className="error__home" onClick={handleHome}>
+            Trang chủ
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
 export default Error404;
