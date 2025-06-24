018a3a4ab162debb5fa4123600777bcf8c7fadc1
Tien Lam <nhattienlam2005@gmail.com>
2025-06-24 16:46:19 +0700

    lấy lai chat model

---
+++ backend/chat/chat.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const chatSchema = new mongoose.Schema(
+  {
+    senderId: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+    seen: {
+      type: Boolean,
+      default: false,
+    },
+    timestamp: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Chat = mongoose.model("Chat", chatSchema, "chats");
+
+module.exports = Chat;
